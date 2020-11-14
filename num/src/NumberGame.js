9ea3e3b4b393a1170a73ad71f6654969c7c7b2fa
unknown <jreidmke@gmail.com>
2020-11-14 13:14:35 -0600

    boiler plate and importing

---
+++ num/src/NumberGame.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const NumberGame = (props) => {
+
+
+    return (
+        <div>
+            <h1>Your Guess: 0</h1>
+            <button>Generate Num</button>
+        </div>
+    )
+}
+
+export default NumberGame;
