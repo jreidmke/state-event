b72667c1d366c32f94d4c6bd8b9c75d59dfadf7c
unknown <jreidmke@gmail.com>
2020-11-14 13:23:12 -0600

    init number game

--- num/src/NumberGame.js
+++ num/src/NumberGame.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 
 const NumberGame = (props) => {
+    const genRandom = () => {
+        return Math.floor(Math.random() * 10) + 1;
+    }
 
+    const [guess, setGuess] = useState(genRandom());
+    const [target, setTarget] = useState(genRandom());
 
     return (
         <div>
-            <h1>Your Guess: 0</h1>
-            <button>Generate Num</button>
+            <h1>Target Num: {target}</h1>
+            <br></br>
+            <h1>Your Guess: {guess}</h1>
+            <button onClick={() => setGuess(genRandom)}>Generate Num</button>
         </div>
     )
 }
