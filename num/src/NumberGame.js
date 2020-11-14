85f657eb86f38e9a413160f1cb6dbe331c9a485c
unknown <jreidmke@gmail.com>
2020-11-14 13:32:39 -0600

    updated styling and button

--- num/src/NumberGame.js
+++ num/src/NumberGame.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import "./NumberGame.css";
 
 const NumberGame = (props) => {
     const genRandom = () => {
@@ -7,13 +8,16 @@ const NumberGame = (props) => {
 
     const [guess, setGuess] = useState(genRandom());
     const [target, setTarget] = useState(genRandom());
+    const isWinner = target === guess;
+    const button = <button onClick={() => setGuess(genRandom)}>Generate Num</button>
 
     return (
         <div>
             <h1>Target Num: {target}</h1>
             <br></br>
-            <h1>Your Guess: {guess}</h1>
-            <button onClick={() => setGuess(genRandom)}>Generate Num</button>
+            <h1 className={isWinner ? 'winner' : 'loser'}>Your Guess: {guess}</h1>
+            {isWinner ? null : button}
+            {/* Look at this this is cool ^^ */}
         </div>
     )
 }
