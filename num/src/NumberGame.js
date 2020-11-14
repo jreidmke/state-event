e446b46f8376de0010af525cd31abc0b045b7ba1
unknown <jreidmke@gmail.com>
2020-11-14 13:43:31 -0600

    added new game feature

--- num/src/NumberGame.js
+++ num/src/NumberGame.js
@@ -6,18 +6,31 @@ const NumberGame = (props) => {
         return Math.floor(Math.random() * 10) + 1;
     }
 
+    const restart= () => {
+        (setTarget(genRandom()));
+        setGuessCount(0);
+    };
+
+    const makeGuess = () => {
+        setGuess(genRandom());
+        setGuessCount(guessCount + 1);
+    }
+
     const [guess, setGuess] = useState(genRandom());
     const [target, setTarget] = useState(genRandom());
+    const [guessCount, setGuessCount] = useState(0);
     const isWinner = target === guess;
-    const button = <button onClick={() => setGuess(genRandom)}>Generate Num</button>
+    const button = <button onClick={() => makeGuess()}>Generate Num</button>
 
     return (
         <div>
             <h1>Target Num: {target}</h1>
             <br></br>
             <h1 className={isWinner ? 'winner' : 'loser'}>Your Guess: {guess}</h1>
+            <h4>Guess Count: {guessCount}</h4>
             {isWinner ? null : button}
             {/* Look at this this is cool ^^ */}
+            <button onClick={restart}>New Game</button>
         </div>
     )
 }
